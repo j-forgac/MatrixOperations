ed434667ee8e88b88329ac5e9e7c7314a65d1ca6
Jan Forgac <jan.forgac@educanet.cz>
2021-02-12 20:35:22 +0100

    done

--- src/main/java/cz/educanet/tranformations/Matrix.java
+++ src/main/java/cz/educanet/tranformations/Matrix.java
@@ -6,73 +6,104 @@ import java.util.Arrays;
 
 public class Matrix implements IMatrix {
 
-    private final double[][] rawArray;
+	private final double[][] rawArray;
 
-    public Matrix(double[][] rawArray) {
-        this.rawArray = rawArray;
-    }
+	public Matrix(double[][] rawArray) {
+		this.rawArray = rawArray;
+	}
 
-    @Override
-    public int getRows() {
-        return rawArray.length;
-    }
+	@Override
+	public int getRows() {
+		return rawArray.length;
+	}
 
-    @Override
-    public int getColumns() {
-        if (getRows() > 0)
-            return rawArray[0].length;
+	@Override
+	public int getColumns() {
+		if (getRows() > 0)
+			return rawArray[0].length;
 
-        return 0;
-    }
+		return 0;
+	}
 
-    @Override
-    public IMatrix times(IMatrix matrix) {
-        throw new NotImplementedError(); // TODO:
-    }
+	@Override
+	public IMatrix times(IMatrix matrix) {
+		if (this.getColumns() == matrix.getRows()) {
+			double[][] newMatrix = new double[this.getRows()][matrix.getColumns()];
+			double tileValue;
+			for (int firstMatrixRow = 0; firstMatrixRow < this.getRows(); firstMatrixRow++) {
+				for (int secondMatrixColumn = 0; secondMatrixColumn < matrix.getColumns(); secondMatrixColumn++) {
+					tileValue = 0.0d;
+					for (int firstMatrixColumn = 0; firstMatrixColumn < this.getColumns(); firstMatrixColumn++) {
+						tileValue += this.get(firstMatrixRow, firstMatrixColumn) * matrix.get(firstMatrixColumn, secondMatrixColumn);
+					}
+					newMatrix[firstMatrixRow][secondMatrixColumn] = tileValue;
+				}
+			}
+			return MatrixFactory.create(newMatrix);
+		}
+		return null;
+	}
 
-    @Override
-    public IMatrix times(Number scalar) {
-        throw new NotImplementedError(); // TODO:
-    }
+	@Override
+	public IMatrix times(Number scalar) {
+		double[][] newMatrix = new double[this.getRows()][this.getColumns()];
+		for (int row = 0; row < this.getRows(); row++) {
+			for (int column = 0; column < this.getColumns(); column++) {
+				newMatrix[row][column] = this.get(row, column) * scalar.doubleValue();
+			}
+		}
+		return MatrixFactory.create(newMatrix);
+	}
 
-    @Override
-    public IMatrix add(IMatrix matrix) {
-        throw new NotImplementedError(); // TODO:
-    }
+	@Override
+	public IMatrix add(IMatrix matrix) {
+		double[][] newMatrix = new double[this.getRows()][this.getColumns()];
+		if (this.getRows() != matrix.getRows() || this.getColumns() != matrix.getColumns()) {
+			return null;
+		} else {
+			for (int row = 0; row < matrix.getRows(); row++) {
+				for (int column = 0; column < matrix.getColumns(); column++) {
+					newMatrix[row][column] = this.get(row, column) + matrix.get(row, column);
+				}
+			}
+			return MatrixFactory.create(newMatrix);
+		}
+	}
 
-    @Override
-    public double get(int n, int m) {
-        throw new NotImplementedError(); // TODO:
-    }
+	@Override
+	public double get(int n, int m) {
+		return this.rawArray[n][m];
+	}
 
-    //region Optional
-    @Override
-    public IMatrix transpose() {
-        return null;
-    }
+	//region Optional
+	@Override
+	public IMatrix transpose() {
+		return null;
+	}
 
-    @Override
-    public double determinant() {
-        return 0;
-    }
-    //endregion
-    //region Generated
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Matrix matrix = (Matrix) o;
+	@Override
+	public double determinant() {
+		return 0;
+	}
 
-        for (int i = 0; i < rawArray.length; i++) {
-            if(!Arrays.equals(rawArray[i], matrix.rawArray[i]))
-                return false;
-        }
-        return true;
-    }
+	//endregion
+	//region Generated
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Matrix matrix = (Matrix) o;
 
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(rawArray);
-    }
-    //endregion
+		for (int i = 0; i < rawArray.length; i++) {
+			if (!Arrays.equals(rawArray[i], matrix.rawArray[i]))
+				return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(rawArray);
+	}
+	//endregion
 }
--- src/test/java/cz/educanet/tranformations/MatrixTest.java
+++ src/test/java/cz/educanet/tranformations/MatrixTest.java
@@ -4,84 +4,142 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class MatrixTest {
 
-    private IMatrix a;
-    private IMatrix b;
-    private IMatrix c;
-    private IMatrix d;
-    private IMatrix empty;
+	private IMatrix a;
+	private IMatrix b;
+	private IMatrix c;
+	private IMatrix d;
+	private IMatrix e;
+	private IMatrix f;
+	private IMatrix g;
+	private IMatrix h;
+	private IMatrix i;
+	private IMatrix res1;
+	private IMatrix res2;
+	private IMatrix empty;
 
-    @Before
-    public void setUp() throws Exception {
-        double[][] rawA = {
-                {1, 1, 1},
-                {1, 1, 1},
+	@Before
+	public void setUp() throws Exception {
+		double[][] rawA = {
+				{1, 1, 1},
+				{1, 1, 1},
+		};
+		a = MatrixFactory.create(rawA);
+
+		double[][] rawB = {
+				{1, 1, 1},
+				{1, 1, 1},
+				{0, 0, 0},
+		};
+		b = MatrixFactory.create(rawB);
+		double[][] rawC = {
+				{1, 0, 0},
+				{0, 1, 0},
+				{0, 0, 1},
+		};
+		c = MatrixFactory.create(rawC);
+
+		double[][] rawEmpty = {};
+		empty = MatrixFactory.create(rawEmpty);
+
+		double[][] rawD = {
+				{1, 1}
+		};
+
+		//*nasobeni
+
+		d = MatrixFactory.create(rawD);
+
+		double[][] rawE = {
+				{2, -1, 3},
+				{3, 0, -2},
+		};
+		e = MatrixFactory.create(rawE);
+
+		double[][] rawF = {
+				{1, 1},
+				{2, -2},
+				{3, 1},
+		};
+		f = MatrixFactory.create(rawF);
+
+		double[][] rawResult = {
+				{5.0, -1.0, 1.0},
+				{-2.0, -2.0, 10.0},
+				{9.0, -3.0, 7.0}
+		};
+		res1 = MatrixFactory.create(rawResult);
+
+        double[][] rawResult2 = {
+                {9,7},
+                {-3,1},
         };
-        a = MatrixFactory.create(rawA);
+        res2 = MatrixFactory.create(rawResult2);
 
-        double[][] rawB = {
-                {1, 1, 1},
-                {1, 1, 1},
-                {0, 0, 0},
-        };
-        b = MatrixFactory.create(rawB);
-        double[][] rawC = {
-                {1, 0, 0},
-                {0, 1, 0},
-                {0, 0, 1},
-        };
-        c = MatrixFactory.create(rawC);
+		//*nasobeni skalarem + scitani
 
-        double[][] rawEmpty = {};
-        empty = MatrixFactory.create(rawEmpty);
 
-        double[][] rawD = {
-                {1, 1}
-        };
-        d = MatrixFactory.create(rawD);
+		double[][] rawG = {
+				{2, -1, 3},
+				{3, 0, -2}
+		};
+		g = MatrixFactory.create(rawG);
+
+		double[][] rawH = {
+				{4, -2, 6},
+				{6, 0, -4}
+		};
+		h = MatrixFactory.create(rawH);
+	}
+
+	@Test
+	public void getRows() {
+		assertEquals(2, a.getRows());
+		assertEquals(3, b.getRows());
+		assertEquals(3, c.getRows());
+		assertEquals(0, empty.getRows());
+	}
+
+	@Test
+	public void getColumns() {
+		assertEquals(3, a.getColumns());
+		assertEquals(3, b.getColumns());
+		assertEquals(3, c.getColumns());
+		assertEquals(0, empty.getColumns());
+	}
+
+	@Test
+	public void times() {
+		assertEquals(res2,e.times(f));
+		assertEquals(res1,f.times(e));
+	}
+
+	@Test
+	public void timesScalar() {
+        assertEquals(h,g.times(2));
     }
 
-    @Test
-    public void getRows() {
-        assertEquals(2, a.getRows());
-        assertEquals(3, b.getRows());
-        assertEquals(3, c.getRows());
-        assertEquals(0, empty.getRows());
-    }
+	@Test
+	public void add() {
+        assertEquals(h,g.times(g));
+	}
 
-    @Test
-    public void getColumns() {
-        assertEquals(3, a.getColumns());
-        assertEquals(3, b.getColumns());
-        assertEquals(3, c.getColumns());
-        assertEquals(0, empty.getColumns());
-        assertEquals(2, d.getColumns());
-    }
+	@Test
+	public void get() {
+		assertEquals(1, (long) a.get(1, 1));
+	}
 
-    @Test
-    public void times() {
-    }
+	@Test
+	public void transpose() {
+	}
 
-    @Test
-    public void timesScalar() {
-    }
-
-    @Test
-    public void add() {
-    }
-
-    @Test
-    public void get() {
-    }
-
-    @Test
-    public void transpose() {
-    }
-
-    @Test
-    public void determinant() {
-    }
+	@Test
+	public void determinant() {
+        //*tohle jindy, nestíhám moc, ale bude to
+	}
 }
